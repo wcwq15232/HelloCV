ae9bf10e2a3a9aaf0d8feeee864855b8b8bf5793
wqwq15232 <zyx1523204816@outlook.com>
2025-10-24 23:01:32 +0800

    第三周作业

---
+++ opencv/traffic_light/s6.cpp
@@ -0,0 +1,90 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+using namespace cv;
+
+bool getContours(Mat& imgDil, Mat& img){
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+
+    findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    // drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
+    bool result = false;
+
+    for (int i = 0; i < contours.size(); ++i){
+        int area = contourArea(contours[i]);
+
+        vector<vector<Point>> conPoly(contours.size());
+        vector<Rect> boundRect(contours.size());
+        string objectType = "";
+        
+        if (area > 30000){
+            result = true;
+            float peri = arcLength(contours[i], true);
+            approxPolyDP(contours[i], conPoly[i], 0.03 * peri, true);
+            drawContours(img, conPoly, i, Scalar(0, 0, 255), 2, LINE_4);
+            cout << contours[i].size() << endl;
+            boundRect[i] = boundingRect(conPoly[i]);
+            rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 2);
+        }
+    }
+    return result;
+}
+
+vector<vector<int>> RG {{133,120,150,179,255,255}, {74, 144, 127, 93, 251, 218}};
+vector<Scalar> BGR {{255, 0, 0}, {0, 255, 0}, {0, 0, 255}};
+map<string, int> map_{{"blue", 0}, {"green", 1}, {"red", 2}};
+
+int main(){
+    string path = "../../Resources/TrafficLight.mp4", objType;
+    VideoCapture cap(path);
+
+    // double fps = cap.get(CAP_PROP_FPS);
+    // Size frameSize(cap.get(CAP_PROP_FRAME_WIDTH), cap.get(CAP_PROP_FRAME_HEIGHT));
+    // VideoWriter writer("output_f.mp4", VideoWriter::fourcc('M', 'P', '4', 'V'), fps, frameSize);
+
+    Mat frame, frame_src;
+    Mat imgHSV;
+    Mat mask_r, mask_y, mask_g, canny_r, canny_y, canny_g;
+    Mat kernel = getStructuringElement(cv::MORPH_RECT, Size(3,3));
+    bool R, G;
+
+    Scalar lower_r(RG[0][0], RG[0][1], RG[0][2]);
+    Scalar upper_r(RG[0][3], RG[0][4], RG[0][5]);
+    Scalar lower_g(RG[1][0], RG[1][1], RG[1][2]);
+    Scalar upper_g(RG[1][3], RG[1][4], RG[1][5]);
+    int re = -1;
+    while(re != 27){
+        cap.read(frame_src);
+        objType = "";
+
+        if (frame_src.empty()) break;
+
+        GaussianBlur(frame_src, frame, Size(7, 7), 0);
+        cvtColor(frame, imgHSV, COLOR_BGR2HSV);
+        inRange(imgHSV, lower_r, upper_r, mask_r);
+        inRange(imgHSV, lower_g, upper_g, mask_g);
+        Canny(mask_r, canny_r, 75, 150);
+        Canny(mask_g, canny_g, 75, 150);
+        dilate(canny_r, canny_r, kernel);
+        dilate(canny_g, canny_g, kernel);
+
+        if(getContours(canny_r, frame_src)) objType = "red";
+        if(getContours(canny_g, frame_src)) objType = "green";
+
+        if (!objType.empty()) putText(frame_src, objType, Point(20, 110), FONT_HERSHEY_DUPLEX, 5, BGR[map_[objType]], 7);
+
+        imshow("src", frame_src);
+        re = waitKey(60);
+        // writer.write(frame_src);
+    }
+    cap.release();
+    // writer.release();
+    destroyAllWindows();
+}
--- readme.md
+++ readme.md
@@ -2,13 +2,19 @@
 
 [cmake study](https://www.yuque.com/yuqueyonghuakgijn/xmbrbw/go4n0p3gpvwskk15)
 
-[实践任务](https://www.yuque.com/yuqueyonghuakgijn/xmbrbw/dv03zgw4ynsuoraq?singleDoc#)
+[实践任务](https://www.yuque.com/yuqueyonghuakgijn/xmbrbw/go4n0p3gpvwskk15)
 
 [docker study](https://www.yuque.com/yuqueyonghuakgijn/xmbrbw/uqfeicvpl0a4vrgq)
 
 [GIT study](https://www.yuque.com/yuqueyonghuakgijn/xmbrbw/xxq9e69lgq0xhv8x?singleDoc#) 
 
 [opencv ros2 环境配置](https://www.yuque.com/yuqueyonghuakgijn/xmbrbw/gv6y7g8eiy43w5gv?singleDoc#)
+
+
+
+[文档矫正](https://www.yuque.com/yuqueyonghuakgijn/xmbrbw/ilvwr4flpgxybf59?singleDoc#) 
+
+[红绿灯识别，虽然说写的有点水哈](https://www.yuque.com/yuqueyonghuakgijn/xmbrbw/kq13erb83414zrgg?singleDoc#) 
 
 开发系统环境`Ubuntu22.04.5` `OpenCV4.5` `ros2桌面版` `cmake 3.22.1`
 
